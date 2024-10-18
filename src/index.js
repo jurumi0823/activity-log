81cd029bc359a84f481766d6a9d59047c7cda004
jurumi0823 <131954979+jurumi0823@users.noreply.github.com>
2024-10-18 04:30:27 +0000

    checking

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const core = require('@actions/core')
 // Main function to execute the update process
 async function main() {
     try {
-        const activity = await fetchAndFilterEvents({ username, token, eventLimit, ignoreEvents });
+        const activity = await fetchAndFilterEvents({ username, token: process.env.ACTIONSECRET2024, eventLimit, ignoreEvents });
         await updateReadme(activity, readmePath);
     } catch (error) {
         core.setFailed(`❌ Error in the update process: ${error.message}`);
